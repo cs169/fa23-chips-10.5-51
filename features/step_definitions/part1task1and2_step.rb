2b0189fbb651121875aa119d9035385d2b79ed59
Yifan Zhu <brucezhu_22@berkeley.edu>
2023-11-25 03:55:17 +0000

    rubocop fix

--- features/step_definitions/part1task1and2_step.rb
+++ features/step_definitions/part1task1and2_step.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 Given('task 1.23 I am on the login page') do
   visit '/login'
@@ -6,7 +7,6 @@ end
 Given('task 1.23 I am on the create article page') do
   visit '/representatives/3/news_items'
 end
-
 
 When('task 1.23 I click {string}') do |button|
   click_button(button)
